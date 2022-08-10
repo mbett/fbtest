278ef181af615a01503eed836f137cf752df8abf
mbett <mbett@cs.cmu.edu>
2022-08-10 15:41:08 -0400

    Update cards.js

--- cards.js
+++ cards.js
@@ -1,4 +1,24 @@
-include('firebase-connector.js');
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDHUkpWdXOICM6_oka9SsNWScuqAUm95hE",
+  authDomain: "best-learning-tool-bd3a0.firebaseapp.com",
+  projectId: "best-learning-tool-bd3a0",
+  storageBucket: "best-learning-tool-bd3a0.appspot.com",
+  messagingSenderId: "784855659403",
+  appId: "1:784855659403:web:7ac0ff09b72481489eb58e",
+  measurementId: "G-30KRCJDD9J"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 let myFlashcards = [ {
     "question": "Name this species of bird.",
