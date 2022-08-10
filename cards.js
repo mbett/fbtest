a369c70983dabf13eb027fbd32fd4444d6b74539
mbett <mbett@cs.cmu.edu>
2022-08-10 16:25:02 -0400

    Update cards.js

--- cards.js
+++ cards.js
@@ -16,6 +16,29 @@ let myFlashcards = [ {
     }
   ];
 
+
+ // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDHUkpWdXOICM6_oka9SsNWScuqAUm95hE",
+    authDomain: "best-learning-tool-bd3a0.firebaseapp.com",
+    projectId: "best-learning-tool-bd3a0",
+    storageBucket: "best-learning-tool-bd3a0.appspot.com",
+    messagingSenderId: "784855659403",
+    appId: "1:784855659403:web:7ac0ff09b72481489eb58e",
+    measurementId: "G-30KRCJDD9J"
+  };
+
+  // Initialize Firebase
+  const firebase = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(firebase);
+
 // Make the database point to the location root -> deck -> flashcards
 // If the location doesn't exist is will be created
 // Create the reference location
